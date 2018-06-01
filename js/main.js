ae5bf61b97d50940217907447f025a36592f1e17
Jelename <yelena.medenica@gmail.com>
2018-06-01 12:55:40 +0200

    new card - taking user input

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ let flashCards = [
     {
       question: 'Can an array store all data types?',
       answer: 'yes, it can :)',
-      right() {},
-      wrong() {},
-    },
+      //right() {},
+      //wrong() {},
+    }
 ]
+
+function submit() {
+  let newQuestion = document.getElementById('newQuestion').value;
+  document.getElementById('newQuestion').value = null;
+  console.log(newQuestion);
+}
